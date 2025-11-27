2cbbf60b710cb57121a865cf6fff2cbf00d15703
Bryan Alvarado <hola@bryanalvarado.es>
2025-11-27 15:53:04 +0100

    Add git ignore

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,17 +1,6 @@
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDekpEFG2v886hNRn0yE0zlaDqbtban53k",
-  authDomain: "vacacioneseuropa.firebaseapp.com",
-  databaseURL: "https://vacacioneseuropa-default-rtdb.firebaseio.com",
-  projectId: "vacacioneseuropa",
-  storageBucket: "vacacioneseuropa.firebasestorage.app",
-  messagingSenderId: "17419236448",
-  appId: "1:17419236448:web:769231fcb6d2bb35463f05",
-  measurementId: "G-GNVKGMXLQR"
-};
+import { firebaseConfig } from './firebaseConfig';
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
